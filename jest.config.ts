2ba25eb32a953d179154657ae78eafd91be689b4
Thomas Dekiere <t.dekiere@gmail.com>
2025-02-06 15:20:47 +0100

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/nvf66YnT3r

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  displayName: 'nx-node-22-typescript-5-8-require-esm',
+  preset: './jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: './coverage/nx-node-22-typescript-5-8-require-esm',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
